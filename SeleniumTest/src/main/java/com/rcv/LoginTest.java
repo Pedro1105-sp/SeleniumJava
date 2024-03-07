aaaabc5832f99180c917868a4ea3ba9f9a3ac313
Pedro Henrique Alves <pedro.henriquerealalvessp@gmail.com>
2024-03-07 10:10:44 -0300

    Testando Login

--- SeleniumTest/src/main/java/com/rcv/LoginTest.java
+++ SeleniumTest/src/main/java/com/rcv/LoginTest.java
@@ -1,7 +1,20 @@
 package com.rcv;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
     public static void main(String[] args) {
 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\pedro.candido\\Desktop\\PEDRO\\chromedriver-win64\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        driver.quit();
     }
 }

8748ea4aff3d9b5ba148b89109445fa3812dba30
Pedro Henrique Alves <pedro.henriquerealalvessp@gmail.com>
2024-03-11 13:29:17 -0300

    Lidando com o CheckBox no Selenium

--- SeleniumTest/src/main/java/com/rcv/CheckBox.java
+++ SeleniumTest/src/main/java/com/rcv/CheckBox.java
@@ -1,7 +1,10 @@
 package com.rcv;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 
 public class CheckBox {
     public static void main(String[] args) throws InterruptedException {
@@ -10,6 +13,11 @@ public class CheckBox {
         driver.manage().window().maximize();
         driver.get("https://www.sugarcrm.com/au/request-demo/");
         Thread.sleep(5000);
+
+        WebElement radio = driver.findElement(By.id("doi0"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(radio);
+        actions.perform();
 
         driver.findElement(By.name("doi")).click();
         Thread.sleep(5000);

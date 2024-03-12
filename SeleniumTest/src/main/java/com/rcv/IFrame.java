0b0daedb47bbb0e5da5efdcfab4c4d886faa0533
Pedro Henrique Alves <pedro.henriquerealalvessp@gmail.com>
2024-03-12 16:33:07 -0300

    Lidando com IFrame, Alert Popup no Selenium

--- SeleniumTest/src/main/java/com/rcv/IFrame.java
+++ SeleniumTest/src/main/java/com/rcv/IFrame.java
@@ -1,5 +1,6 @@
 package com.rcv;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,11 +10,21 @@ public class IFrame {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\pedro.candido\\Desktop\\PEDRO\\chromedriver-win64\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert");
+        //driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert");
+        driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt");
 
-        WebElement frame1 = driver.findElement(By.id("frameElement"));
+        WebElement frame1 = driver.findElement(By.id("iframeResult"));
         driver.switchTo().frame(frame1);
         driver.findElement(By.xpath("/html/body/button")).click();
+        String alertText = driver.switchTo().alert().getText();
+        //System.out.println(alertText);
+        Thread.sleep(5000);
+
+        Alert alertonpage = driver.switchTo().alert();
+        alertonpage.sendKeys("PEDRO TESTE");
+        alertonpage.accept();
+
+        //driver.switchTo().alert().accept();
         driver.switchTo().parentFrame();
         System.out.println(driver.getTitle());
     }
